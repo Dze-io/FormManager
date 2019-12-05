762b1fec329a20553d265fd5bb5715e1a7e04c3d
Avior <florian.bouillon@delta-wings.net>
2019-12-05 09:54:00 +0100

    Added new input !
    Signed-off-by: Avior <florian.bouillon@delta-wings.net>

---
+++ src/modules/CheckboxInput.ts
@@ -0,0 +1,30 @@
+import InputIdentity from './Interfaces/InputIdentity';
+import DefaultInput from './DefaultInput';
+import { toBoolean } from '../Functions';
+
+/**
+ *
+ * @class FMDateInput
+ * @extends {FMInput}
+ */
+export default class CheckboxInput extends DefaultInput {
+
+	public setValue(value: any) {
+		this.element.checked = this.formatValue(value)
+	}
+	public getValue(): boolean {
+		return this.element.checked
+	}
+	public formatValue(value: any): boolean {
+		value = toBoolean(value)
+		if (typeof value === "undefined") {
+			return false
+		}
+		return value
+	}
+
+	public static identity: InputIdentity = {
+		input: CheckboxInput,
+		type: "checkbox"
+	}
+}
