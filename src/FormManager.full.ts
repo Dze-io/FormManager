b61cb6b646b9b2346a003966d84e401667ba9a75
Avior <florian.bouillon@delta-wings.net>
2019-12-05 09:56:31 +0100

    Added a self containing version of FormManager
    Signed-off-by: Avior <florian.bouillon@delta-wings.net>

---
+++ src/FormManager.full.ts
@@ -0,0 +1,40 @@
+import FormManager from './FormManager'
+import DatalistInput from './modules/DatalistInput'
+import DateInput from './modules/DateInput'
+import RepeatInput from './modules/RepeatInput'
+import SelectInput from './modules/SelectInput'
+import RegexAttribute from './attributes/RegexAttribute'
+import IgnoreAttribute from './attributes/IgnoreAttribute'
+import DefaultAttribute from './attributes/DefaultAttribute'
+import AutosetAttribute from './attributes/AutosetAttribute'
+import checkboxInput from './modules/CheckboxInput'
+
+/**
+ * This class is Mainly used for (non-npm) browser usage as it contains every buitins extensions
+ *
+ * @export
+ * @class fm
+ * @extends {FormManager}
+ */
+export default class fm extends FormManager {
+	public constructor(form: HTMLFormElement) {
+		super(form)
+		this.assign(
+			DatalistInput,
+			DateInput,
+			RepeatInput,
+			SelectInput,
+			checkboxInput,
+		)
+		this.setupInputs()
+		this.attributeManager.register(
+			RegexAttribute,
+			IgnoreAttribute,
+			DefaultAttribute,
+			AutosetAttribute,
+		)
+		this.attributeManager.setup()
+	}
+}
+
+(window as any).FormManager = fm
